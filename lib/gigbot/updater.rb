6e9e704287e6d6d3b6560b523510dfb62cce14a6
Teejay VanSlyke <root@teejayvanslyke.com>
2023-12-11 07:18:00 -0500

    Handles timeouts

--- lib/gigbot/updater.rb
+++ lib/gigbot/updater.rb
@@ -8,12 +8,21 @@ module Gigbot
 
     def run
       Gigbot::Source.each do |source|
-        source.import
-        puts [
-          "✓".colorize(color: :green),
-          "(#{source.imported.length})".ljust(7, ' ').colorize(color: :yellow),
-          source.parser.title.colorize(color: :blue)
-        ].join(' ')
+        begin
+          source.import
+          puts [
+            "✓".colorize(color: :green),
+            "(#{source.imported.length})".ljust(7, ' ').colorize(color: :yellow),
+            source.parser.title.colorize(color: :blue)
+          ].join(' ')
+        rescue Exception => e
+          puts [
+            "X".colorize(color: :red),
+            "(X)".ljust(7, ' ').colorize(color: :red),
+            source.url.colorize(color: :red),
+            e,
+          ].join(' ')
+        end
       end
     end
   end
